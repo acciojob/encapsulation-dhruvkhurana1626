c5d079eeb4d49229e0964bf992626fa198aa3c27
dhruv <dhruvkhurana162@gmail.com>
2025-10-27 00:03:08 +0530

    Solution1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r1 = new RWOnly();
+        //r1.name = "dhruv";
+        //not able to access name direclty bcz name variable is prvate and can only be accessed inside the same class
+        //now lets use getter and setters to do so
+        r1.setName("dhruv");
+        System.out.print(r1.getName());
+    }
 }
+
